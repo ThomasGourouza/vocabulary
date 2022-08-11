f0754511d0a7e05023f6382346d8f78bea4c6d20
Thomas Gourouza <thomas.gourouza@de.markant.com>
2022-08-11 16:27:29 +0200

    update danish

--- src/app/services/reader-speaker.service.ts
+++ src/app/services/reader-speaker.service.ts
@@ -8,29 +8,45 @@ export class ReaderSpeakerService {
 
   private readonly apiKey: string;
   private readonly baseUrl: string;
-  
+
   constructor(
     private readonly http: HttpClient
   ) {
-    this.apiKey = '874fd7a58e9759571ef1e39daa42c316';
-    this.baseUrl = 'https://scapi-eu.readspeaker.com/a/speak';
+    this.apiKey = 'f56b242feemsh5739e24d3615074p140966jsnbd5c78425fff';
+    this.baseUrl = `https://voicerss-text-to-speech.p.rapidapi.com/?key=${this.apiKey}`;
   }
 
   getVoice(text: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}`, {
-      params: { 
-        key: this.apiKey,
-        lang: 'da_dk',
-        voice: 'Lene',
-        volume: '200',
-        text,
-      },
-    }).pipe(
+    const body = new URLSearchParams();
+    body.append("f", "8khz_8bit_mono");
+    body.append("c", "mp3");
+    body.append("r", "0");
+    body.append("hl", "da-DK");
+    body.append("src", text);
+
+    // const bod = {
+    //   "f": "8khz_8bit_mono",
+    //   "c": "mp3",
+    //   "r": "0",
+    //   "hl": "da-DK",
+    //   "src": text
+    // };
+
+    const headers: { [header: string]: string; } = {
+      'content-type': 'application/x-www-form-urlencoded',
+      'X-RapidAPI-Key': this.apiKey,
+      'X-RapidAPI-Host': 'voicerss-text-to-speech.p.rapidapi.com'
+    };
+
+    return this.http.post(`${this.baseUrl}`, body, headers).pipe(
       retry(1),
-      catchError((error: HttpErrorResponse) => 
-        of(error.url)
-      )
+      catchError((error: HttpErrorResponse) => {
+        console.log(error);
+        
+        return of('error');
+      })
     );
   }
+
 
 }
