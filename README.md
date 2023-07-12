7729cc2d3c97ca8f9080000140266870c011b245
Thomas Gourouza <thomas.gourouza@de.markant.com>
2023-07-12 22:10:04 +0200

    implement kuroshiro

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Vocabulary
 ng build --base-href="/vocabulary/"
+
+Content from '/node_modules/kuromoji/dict' should be copied to 'src/assets/dict'.
--- src/app/pipes/kuroshiro.ts
+++ src/app/pipes/kuroshiro.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
-// import Kuroshiro from "kuroshiro";
-// import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
+import Kuroshiro from "kuroshiro";
+import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
@@ -8,23 +8,23 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 })
 export class KuroshiroPipe implements PipeTransform {
 
-  // private kuroshiro = new Kuroshiro();
-  // private kuroshiroInitDict: Promise<void>;
+  private kuroshiro = new Kuroshiro();
+  private kuroshiroInitDict: Promise<void>;
 
   constructor(private sanitizer: DomSanitizer) {
-    // this.kuroshiroInitDict = this.kuroshiro.init(new KuromojiAnalyzer({ dictPath: 'assets/dict' }));
+    this.kuroshiroInitDict = this.kuroshiro.init(new KuromojiAnalyzer({ dictPath: 'assets/dict' }));
   }
 
   transform(text: string, language: string): Promise<SafeHtml> {
-    // if (language.toLocaleLowerCase() !== 'japanese') {
+    if (language.toLocaleLowerCase() !== 'japanese') {
       return new Promise<SafeHtml>(resolve =>
         resolve(this.sanitizer.bypassSecurityTrustHtml(text))
       );
-    // }
-    // return this.kuroshiroInitDict.then(() => {
-    //   return this.kuroshiro.convert(text, { to: 'hiragana', mode: 'furigana' });
-    // }).then((htmlCode) =>
-    //   this.sanitizer.bypassSecurityTrustHtml(htmlCode)
-    // );
+    }
+    return this.kuroshiroInitDict.then(() => {
+      return this.kuroshiro.convert(text, { to: 'hiragana', mode: 'furigana' });
+    }).then((htmlCode) =>
+      this.sanitizer.bypassSecurityTrustHtml(htmlCode)
+    );
   }
 }
